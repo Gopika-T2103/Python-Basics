a5aedc6343c3eb964abce820b0d7ace102abd34f
Gopika T <gopikavenugopal03@gmail.com>
2025-09-11 21:03:35 +0530

    Update function.py

--- function.py
+++ function.py
@@ -125,13 +125,3 @@ print(factnum)
 
 print('----------------------------------------------------')
 
-def oddeven(num):
-    if num%2==0:
-        return num,' is even'
-    else:
-       return num,' is odd'
-n1=int(input('enter the number1= '))
-oddevennum=oddeven(n1)
-print(oddevennum)
-
-print('----------------------------------------------------')
