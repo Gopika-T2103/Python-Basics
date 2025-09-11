48f36cf449a5d0d95c4ba781a1a9047b877a2057
Gopika T <gopikavenugopal03@gmail.com>
2025-09-11 21:02:31 +0530

    Update oddeven.py

--- oddeven.py
+++ oddeven.py
@@ -1,5 +1,9 @@
-n=int(input('enter the number = '))
-if (n%2==0):
-    print(n,"is an even number")
-else:
-    print(n,"is an odd number")
+
+def oddeven(num):
+    if num%2==0:
+        return num,' is even'
+    else:
+       return num,' is odd'
+n1=int(input('enter the number1= '))
+oddevennum=oddeven(n1)
+print(oddevennum)
